80f34cec8a1f39be02975874305abcbc4822ce62
Erik Borges <erikborgesofficial@gmail.com>
2025-06-14 03:42:34 -0300

    Inicio de atividade assincrona

---
+++ scripts.js
@@ -0,0 +1,29 @@
+const uploadBtn = document.getElementById("upload-btn");
+const inputUpload = document.getElementById("image-upload")
+
+uploadBtn.addEventListener("click", () => {
+    inputUpload.click();
+})
+
+function lerConteudoDoArquivo(arquivo) {
+    return new Promise((resolve, reject) => {
+        const leitor = new FileReader();
+        leitor.onload = () => {
+            resolve({url: leitor.result, nome: arquivo.name});
+        }
+        leitor.onerror = () => {
+            reject(`Não foi possível ler o conteúdo do arquivo ${arquivo.name}`);
+        }
+        leitor.readAsDataURL(arquivo);
+    })
+}
+
+const imagemPrincipal = document.querySelector(".main-imagem");
+const nomeDaImagem = document.querySelector(".container-imagem-nome p");
+
+inputUpload.addEventListener("change", async(evento) => {
+    const arquivo = evento.target.files[0];
+    if (arquivo) {
+        
+    }
+})
